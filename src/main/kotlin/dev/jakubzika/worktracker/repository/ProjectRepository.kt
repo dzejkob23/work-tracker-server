706ea07b73bd00a792eeb5f9026b85459f266c66
Jakub Zika <kuba.zika@seznam.cz>
2020-07-25 22:00:20 +0200

    Project repository created.

---
+++ src/main/kotlin/dev/jakubzika/worktracker/repository/ProjectRepository.kt
@@ -0,0 +1,48 @@
+package dev.jakubzika.worktracker.repository
+
+import dev.jakubzika.worktracker.db.Schema.Project
+import dev.jakubzika.worktracker.db.Schema.Projects
+import dev.jakubzika.worktracker.extension.dbQuery
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.statements.InsertStatement
+
+interface ProjectRepository {
+    suspend fun addProject(
+            name: String,
+            startTime: String,
+            endTime: String,
+            userId: Int
+    ): Project?
+    suspend fun findProject(projectId: Int): Project?
+    suspend fun findUsersProjects(userId: Int): List<Project>
+}
+
+class ProjectRepositoryImpl : ProjectRepository {
+
+    override suspend fun addProject(
+            name: String,
+            startTime: String,
+            endTime: String,
+            userId: Int
+    ): Project? {
+        var statement: InsertStatement<Number>? = null
+        dbQuery {
+            statement = Projects.insert { project ->
+                project[Projects.name] = name
+                project[Projects.startTime] = startTime
+                project[Projects.endTime] = endTime
+                project[Projects.userId] = userId
+            }
+        }
+        return Projects.toProject(statement?.resultedValues?.get(0) ?: return null)
+    }
+
+    override suspend fun findProject(projectId: Int): Project? = dbQuery {
+        Projects.select { Projects.id.eq(projectId) }.map { Projects.toProject(it) }.singleOrNull()
+    }
+
+    override suspend fun findUsersProjects(userId: Int): List<Project> {
+        return Projects.select { Projects.userId.eq(userId) }.map { Projects.toProject(it) }
+    }
+}
--- src/main/kotlin/dev/jakubzika/worktracker/repository/UserRepository.kt
+++ src/main/kotlin/dev/jakubzika/worktracker/repository/UserRepository.kt
@@ -8,14 +8,11 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
 interface UserRepository {
-    suspend fun findUser(userId: Int): User?
     suspend fun addUser(nickname: String, password: String): User?
+    suspend fun findUser(userId: Int): User?
 }
 
 class UserRepositoryImpl : UserRepository {
-    override suspend fun findUser(userId: Int): User? = dbQuery {
-        Users.select { Users.id.eq(userId) }.map { Users.toUser(it) }.singleOrNull()
-    }
 
     override suspend fun addUser(nickname: String, password: String): User? {
         var statement: InsertStatement<Number>? = null
@@ -26,5 +23,9 @@ class UserRepositoryImpl : UserRepository {
             }
         }
         return Users.toUser(statement?.resultedValues?.get(0) ?: return null)
+    }
+
+    override suspend fun findUser(userId: Int): User? = dbQuery {
+        Users.select { Users.id.eq(userId) }.map { Users.toUser(it) }.singleOrNull()
     }
 }
