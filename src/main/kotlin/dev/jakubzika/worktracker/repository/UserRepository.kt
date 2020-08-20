c225a3b160d9e5d53fb7083cb21e2587f8e544b5
Jakub Zika <kuba.zika@seznam.cz>
2020-08-20 16:32:42 +0200

    Added findUser method.

--- src/main/kotlin/dev/jakubzika/worktracker/repository/UserRepository.kt
+++ src/main/kotlin/dev/jakubzika/worktracker/repository/UserRepository.kt
@@ -10,6 +10,7 @@ import org.jetbrains.exposed.sql.statements.InsertStatement
 interface UserRepository {
     suspend fun addUser(nickname: String, password: String): User?
     suspend fun findUser(userId: Int): User?
+    suspend fun findUser(nickname: String): User?
 }
 
 class UserRepositoryImpl : UserRepository {
@@ -27,5 +28,9 @@ class UserRepositoryImpl : UserRepository {
 
     override suspend fun findUser(userId: Int): User? = dbQuery {
         Users.select { Users.id.eq(userId) }.map { Users.toUser(it) }.singleOrNull()
+    }
+
+    override suspend fun findUser(nickname: String): User? = dbQuery {
+        Users.select { Users.nickname.eq(nickname) }.map { Users.toUser(it) }.singleOrNull()
     }
 }
