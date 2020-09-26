327f00f87f0a76750138742e5c632689841bdd01
Jakub Zika <kuba.zika@seznam.cz>
2020-09-26 18:44:21 +0200

    Added first template.

---
+++ src/main/kotlin/dev/jakubzika/worktracker/pages/template/MainTemplate.kt
@@ -0,0 +1,19 @@
+package dev.jakubzika.worktracker.pages.template
+
+import dev.jakubzika.worktracker.routing.PAGE_TITLE
+import io.ktor.html.*
+import kotlinx.html.*
+
+class MainTemplate : Template<HTML> {
+
+    val content = Placeholder<HtmlBlockTag>()
+
+    override fun HTML.apply() {
+        head {
+            title(PAGE_TITLE)
+        }
+        body {
+            insert(content)
+        }
+    }
+}
