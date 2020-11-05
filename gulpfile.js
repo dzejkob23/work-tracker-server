de9b118e2accc7dd860d98ca3f71a28133e2037b
Jakub Zika <kuba.zika@seznam.cz>
2020-11-05 21:41:34 +0100

    Created gulpfile.js with functions "default" & "build".

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { series, parallel } = require('gulp');
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
+
+var sassPath = './src/main/resources/static/sass/';
+var cssPath = './src/main/resources/static/css';
+
+var sassFiles = sassPath.concat('*.scss');
+var cssFiles = cssPath.concat('*.css');
+
+function style() {
+    return gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssPath));
+}
+
+function minifyStyle() {
+    return gulp.src(cssFiles)
+        .pipe(uglifycss({
+          "uglyComments": true
+        }))
+        .pipe(gulp.dest(cssPath));
+}
+
+function watch() {
+    gulp.watch(sassFiles, ['sass']);
+    gulp.watch(cssFiles, ['css']);
+}
+
+exports.build = series(style, minifyStyle)
+exports.default = series(style, minifyStyle, watch)
